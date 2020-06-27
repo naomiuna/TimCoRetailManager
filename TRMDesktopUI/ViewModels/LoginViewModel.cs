4ec9a843530d65943f58eb0463cc372ea7e8b936
Naomi Todd <naomiunatodd@gmail.com>
2020-06-27 15:58:47 +0100

    Login Form Error Handling

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -43,6 +43,37 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+
+        public bool IsErrorVisible 
+        {
+            get
+            {
+                bool output = false;
+                if(ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+          
+            
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorMessage);
+                NotifyOfPropertyChange(() => IsErrorVisible);
+               
+            }
+        }
+
+
         public bool CanLogIn
         {
             get
@@ -63,12 +94,14 @@ namespace TRMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
+                
             }
             catch (Exception ex)
             {
 
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
 
             }
         }
