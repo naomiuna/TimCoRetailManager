46072833fc8a579da1363760af9c2e413645c594
Naomi Todd <naomiunatodd@gmail.com>
2020-07-22 17:52:27 +0100

    Display Product Data

---
+++ TRMData/dbo/Stored Procedures/spProduct_GetAll.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spProduct_GetAll]
+AS
+BEGIN
+	set nocount on;
+	SELECT Id, ProductName, [Description], RetailPrice, QuantityInStock
+	FROM dbo.Product
+	ORDER BY ProductName;
+END
