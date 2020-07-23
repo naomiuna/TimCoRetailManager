776101a7325041f7f939a8342ad7ae18c22e7911
Naomi Todd <naomiunatodd@gmail.com>
2020-07-23 15:44:05 +0100

    Update Cart Quantities
    Added Automapper and INPC to enable
    cart quantity changes to be reflected

--- TRMDesktopUI.Library/Models/CartItemModel.cs
+++ TRMDesktopUI.Library/Models/CartItemModel.cs
@@ -10,11 +10,6 @@ namespace TRMDesktopUI.Library.Models
     {
         public ProductModel Product { get; set; }
         public int QuantityInCart { get; set; }
-        public string DisplayText {
-            get
-            {
-                return $"{Product.ProductName} ({QuantityInCart})";
-            }
-        }
+     
     }
 }
